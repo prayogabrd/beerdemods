496ed88d48a3dea12243244cb7ceb176419202b3
Prayoga BRD <103347873+prayogabrd@users.noreply.github.com>
2023-01-24 06:40:57 +0700

    Menambahkan fitur hapus profil

--- __use/_del_profile.php
+++ __use/_del_profile.php
@@ -6,6 +6,12 @@ header("Content-Type: application/json; charset=utf-8");
 require("../assets/php/.func__get_user_data.php");
 require("../assets/php/.mysql__funcs.php");
 
+// Ambil data dari fetch
+$input = json_decode(file_get_contents("php://input"), true);
+$result = [
+  "status" => -1,
+];
+
 // Cek apakah sudah pernah masuk sebelumnya
 $_data = get_main_data();
 
@@ -16,6 +22,14 @@ if ($_data != false) {
   $user_pass = $_data["user_pass"];
   $user_account = mysqli_get_data("SELECT `user_id` FROM `account` WHERE `user_name` = '".$user_name."' AND `user_email` = '".$user_email."' AND `user_pass` = '".$user_pass."'");
   if (isset($user_account[0])) {
-    mysqli_query($_conn, "");
+    mysqli_query($_conn, "UPDATE `account` SET `user_profile` = '-' WHERE `user_id` = '".$user_account[0]["user_id"]."'");
+    if (mysqli_affected_rows($_conn) > 0) {
+      $result["status"] = 1;
+    } else {
+      $result["status"] = 0;
+    }
   }
 }
+
+// Kembalikan nilai
+echo(json_encode($result));
